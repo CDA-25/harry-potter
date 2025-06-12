db77990edfc6ff15c7ebdda4a81874b90213d75f
Alexis <desousafreitasalexis@gmail.com>
2025-06-12 15:16:05 +0200

    jeudi 15:16

--- harrypotter/src/main.ts
+++ harrypotter/src/main.ts
@@ -1,1 +1,39 @@
 import './style.css'
+const cardContainer = document.getElementById('card-container')
+
+class HpCharacter {
+  name: string;
+  image: string;
+  house: string;
+  patronus: string;
+  actor: string;
+  alive: boolean
+
+  constructor(name: string, image: string, house: string, patronus: string, actor: string, alive: boolean) {
+    this.name = name;
+    this.image = image;
+    this.house = house;
+    this.patronus = patronus;
+    this.actor = actor;
+    this.alive = alive;
+  }
+}
+
+function showCard() {
+    fetch('https://potterhead-api.vercel.app/api/characters')
+    .then(response => response.json())
+    .then(data => {
+        const card = document.createElement('div')
+        card.className = ''
+        cardContainer?.appendChild(card)
+
+        const name = document.createElement('h2')
+        name.textContent = data.name
+        card.appendChild(name)
+    })
+    .catch(error => console.error('Erreur :', error));
+
+    
+}
+
+showCard()
