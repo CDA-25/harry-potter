6d30eba86f96f818d87662f978372ea31d83efff
Alexis <desousafreitasalexis@gmail.com>
2025-06-12 13:29:24 +0200

    ça marche ouuuu ?

--- harrypotter/vite.config.js
+++ harrypotter/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   server: {
-    port: 3000,
+    port: 5173,
     host: "0.0.0.0",
     strictPort: true,
   }
