fd0fb1ac4cc3039338dc84a281de1ba46202685e
NicolasBondenet <36343149+Vaquicraft@users.noreply.github.com>
2025-06-15 13:59:57 +0200

    ça prend forme

--- hpnicolas/src/main.ts
+++ hpnicolas/src/main.ts
@@ -64,7 +64,11 @@ switch (currentURL) {
     let homePage = document.createElement("div")
     homePage.id = "homePage"
     homePage.classList.add(
-      "bg-red-800"
+      "bg-red-800",
+      "h-[300px]",
+      "flex",
+      "items-center",
+      "justify-center"
     )
     homePage.textContent = "c'est l'accueil."
     document.body.appendChild(homePage)
@@ -84,17 +88,38 @@ switch (currentURL) {
       name: string;
       birth: string;
       gender: string;
+      image: string;
+      house: string;
+      species: string;
+      isWizard: boolean;
 
-      constructor(name: string, birth: string, gender: string) {
+      constructor(
+        name: string, 
+        birth: string, 
+        gender: string,
+        image: string,
+        house: string,
+        species: string,
+        isWizard: boolean
+      ) 
+      {
         this.name = name;
         this.birth = birth;
         this.gender = gender;
+        this.image = image
+        this.house = house
+        this.species = species
+        this.isWizard = isWizard
       }
 
       display(): void {
         let name: string = this.name
         let birth: string = this.birth
         let gender: string = this.gender
+        let image: string = this.image
+        let house: string = this.house
+        let species: string = this.species
+        let isWizard: boolean = this.isWizard
 
         
         let divPerso = document.createElement("div")
@@ -107,11 +132,21 @@ switch (currentURL) {
           "justify-around",
           "flex-wrap",
           "items-center",
-          "w-[200px]",
-          "h-[250px]",
+          "w-[300px]",
+          "h-[400px]",
           "rounded",
           "m-10"
         )
+
+        // function createDOMElement(type: string, id: string, content: string, parent: HTMLElement) {
+        //   let element = document.createElement(type)
+        //   element.id = id
+        //   element.textContent = content
+        //   parent.appendChild(element)
+        // }
+
+        // createDOMElement("div", "houseContent", this.house, document.getElementById("perso")!)
+
 
           let nameContent = document.createElement("div")
           nameContent.id = "nameContent"
@@ -127,6 +162,30 @@ switch (currentURL) {
           genderContent.id = "genderContent"
           genderContent.textContent = gender
           divPerso.appendChild(genderContent)
+
+          let speciesContent = document.createElement("div")
+          speciesContent.id = "speciesContent"
+          speciesContent.textContent = species
+          divPerso.appendChild(speciesContent)
+
+          let houseContent = document.createElement("div")
+          houseContent.id = "houseContent"
+          houseContent.textContent = house
+          divPerso.appendChild(houseContent)
+
+          let imageContent = document.createElement("div")
+          imageContent.id = "imageContent"
+
+          let img = document.createElement("img")
+          img.id = "img"
+          img.classList.add(
+            "rounded-[10px]",
+            "w-[200px]",
+            "h-[250px]"
+          )
+          img.src = image
+          imageContent.appendChild(img)
+          divPerso.appendChild(imageContent)
         
           document.body.appendChild(divPerso)
       }
@@ -143,14 +202,92 @@ switch (currentURL) {
 
       const persos: Perso[] = rawChars.map((char: any) => {
         if (!char.dateOfBirth) {
-          char.dateOfBirth = "inkonuanh"
+          char.dateOfBirth = null
         }
         
-        if (!char.gender) {
-          char.gender = "inkonuanh"
-        }  
+     
         
-        return new Perso(char.name, char.dateOfBirth, char.gender);
+        //   switch (char.gender) {
+        //   case "male": char.gender = "Homme" ; break
+        //   case "female": char.gender = "Femme" ; break
+        //   default: char.gender = null
+        // }
+
+        
+
+        switch (char.gender) {
+          case "male": char.gender = "Homme" ; break
+          case "female": char.gender = "Femme" ; break
+          default: char.gender = null
+        }
+
+        switch (char.house) {
+          case "Gryffindor": char.house = "Gryffondor" ; break
+          case "Hufflepuff": char.house = "Pouffsoufle" ; break
+          case "Ravenclaw": char.house = "Serdaigle" ; break
+          case "Slytherin": char.house = "Serpentard" ; break
+          default: char.house = null ; break
+          
+        }
+
+        switch (char.species) {
+          case "human": char.species = "Humain" ; break
+          case "half-giant": char.species = "Demi-Géant" ; break
+          case "werewolf": char.species = "Loup-Garou" ; break
+          case "cat": char.species = "Chat" ; break
+          case "goblin": char.species = "Gobelin" ; break
+          case "ghost": char.species = "Fantôme" ; break
+          case "half-human": char.species = "Demi-Humain" ; break
+          case "elf": char.species = "Elfe" ; break
+          case "centaur": char.species = "Centaure" ; break
+          case "giant": char.species = "Géant" ; break
+          case "house-elf": char.species = "Elfe de Maison" ; break
+          case "hat": char.species = "Choixpeau" ; break
+          case "hippogriff": char.species = "Hippogriffe" ; break
+          case "arcomentula": char.species = "Arcomentule" ; break
+          case "serpent": char.species = "Basilik" ; break
+          case "cephalopod": char.species = "Céphalopode" ; break
+          case "dragon": char.species = "Wyverne" ; break
+          case "selkie": char.species = "Selkie" ; break
+          case "three-headed dog": char.species = "Iench à trois têtes" ; break
+          case "dog": char.species = "Chien" ; break
+          case "poltergeist": char.species = "Esprit-Frappeur" ; break
+          case "toad": char.species = "Crapeau" ; break
+          case "owl": char.species = "Chouette" ; break
+          case "snake": char.species = "Serpent" ; break
+          default: null
+        }
+
+        if (!char.image) {
+          switch (char.gender) {
+            case "Femme": {
+              if (char.wizard === true) {
+                char.image = "https://media.istockphoto.com/id/1409060868/fr/vectoriel/sorci%C3%A8re-volant-sur-un-balai-sur-fond-de-pleine-lune.jpg?s=612x612&w=0&k=20&c=E8XFzc27ACKH5utYgfQu6btbhk8yapZXtXa6qvIZ0Hc="
+              } else {
+                char.image = "https://previews.123rf.com/images/jemastock/jemastock1712/jemastock171207459/91868647-young-woman-profile-cartoon-icon-vector-illustration-graphic-design.jpg"
+                
+              } break
+            } case "Homme" : {
+              if (char.wizard === true) {
+                char.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdEr1REhhYBVYKVC_lu6X_-JWU2ghqzRyBvw&s"
+              } else {
+                char.image = "https://previews.123rf.com/images/jemastock/jemastock1712/jemastock171201347/90912902-young-man-happy-cartoon-icon-vector-illustration-graphic-design.jpg"
+              }
+            } break
+            default: char.image = "https://media.istockphoto.com/id/1023347350/fr/photo/point-dinterrogation-3d-point-rouge-dinterrogation-demandant-signe-de-ponctuation-isol%C3%A9e.jpg?s=612x612&w=0&k=20&c=eVZrCH5I73a5W_2TZ0tlrWdK68UAoXZPaytoZyGoj90="
+          }
+          
+
+          
+          
+          
+       
+          
+        }
+
+
+        console.log(char)
+        return new Perso(char.name, char.dateOfBirth, char.gender, char.image, char.house, char.species, char.wizard);
       });
 
       persos.forEach(p => p.display());
